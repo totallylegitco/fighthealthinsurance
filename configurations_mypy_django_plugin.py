799d01a14772371c504f5a299ab583af4ce6b1c8
Holden Karau <holden@pigscanfly.ca>
2023-02-13 00:32:58 -0800

    Configure mypy import per https://github.com/typeddjango/django-stubs/pull/180

---
+++ configurations_mypy_django_plugin.py
@@ -0,0 +1,13 @@
+# configurations_mypy_django_plugin.py
+import os
+from configurations.importer import install
+from mypy.version import __version__
+
+from mypy_django_plugin import main
+
+
+def plugin(version):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fighthealthinsurance.config")
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+    install()
+    return main.plugin(version)
