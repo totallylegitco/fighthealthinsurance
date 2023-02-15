3d4d92b16ee76a98299e51b5e28b1cd6e66c6c24
Holden Karau <holden@pigscanfly.ca>
2023-02-14 22:02:09 -0800

    Get scriptmagic integrated

---
+++ fighthealthinsurance/templatetags/absolute.py
@@ -0,0 +1,9 @@
+from django import template
+from django.shortcuts import reverse
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def absolute_url(context, view_name, *args, **kwargs):
+    request = context['request']
+    return request.build_absolute_uri(reverse(view_name, args=args, kwargs=kwargs))
--- fighthealthinsurance/views.py
+++ fighthealthinsurance/views.py
@@ -13,4 +13,38 @@ class ScanView(View):
     def get(self, request):
         return render(
             request,
-            'image_recognize.html')
+            'image_recognize.html',
+            context={
+                'title': "Scan Your Health Insurance Denial"
+            })
+
+
+class PrivacyPolicyView(View):
+    def get(self, request):
+        return render(
+            request,
+            'privacy_policy.html',
+            context={
+                'title': "Privacy Policy"
+            })
+
+
+class OptOutView(View):
+    def get(self, request):
+        return render(
+            request,
+            'opt_out.html',
+            context={
+                'title': "Opt Out",
+            })
+
+
+
+class RemoveDataView(View):
+    def get(self, request):
+        return render(
+            request,
+            'remove_data.html',
+            context={
+                'title': "Remove My Data",
+            })
