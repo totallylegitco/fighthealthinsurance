86c52ad2983b48ff381b61b8fff2563641e8833b
Holden Karau <holden@pigscanfly.ca>
2023-09-13 17:17:08 -0700

    Fix zip code and make a new image

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -10,6 +10,8 @@ fi
 # Start gunicorn
 export DJANGO_CONFIGURATION=${DJANGO_CONFIGURATION:-"Prod"}
 export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-"fighthealthinsurance.settings"}
+# Needed for https://github.com/MacHu-GWU/uszipcode-project/blob/21242cad7cbb7eaf73235086b89499bd90c36531/uszipcode/db.py#L23
+export HOME=$(pwd)
 gunicorn fighthealthinsurance.wsgi --user www-data --bind 0.0.0.0:8010 --workers 4 2>&1 | grep -v kube-probe &
 # And nginx to proxy & serve static files
 nginx -g "daemon off;" 2>&1 |grep -v kube-proxy
