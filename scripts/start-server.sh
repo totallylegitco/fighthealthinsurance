5655ca0c2e1874ae4a90946a980745896b993922
Holden Karau <holden@pigscanfly.ca>
2024-07-20 15:27:09 -0700

    Closer to json

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -12,7 +12,7 @@ export DJANGO_CONFIGURATION=${DJANGO_CONFIGURATION:-"Prod"}
 export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-"fighthealthinsurance.settings"}
 # Needed for https://github.com/MacHu-GWU/uszipcode-project/blob/21242cad7cbb7eaf73235086b89499bd90c36531/uszipcode/db.py#L23
 export HOME=$(pwd)
-gunicorn fighthealthinsurance.wsgi --user www-data --bind 0.0.0.0:8010 --workers 4 --access-logformat  "{'actual_ip': '%({X-Forwarded-For})', 'remote_ip':'%(h)s','request_id':'%({X-Request-Id}i)s','response_code':'%(s)s','request_method':'%(m)s','request_path':'%(U)s','request_querystring':'%(q)s','request_timetaken':'%(D)s','response_length':'%(B)s'}"
+gunicorn fighthealthinsurance.wsgi --user www-data --bind 0.0.0.0:8010 --workers 4 --access-logformat  "{\"actual_ip\": \"%({X-Forwarded-For})\", \"remote_ip\":\"%(h)s\",\"request_id\":\"%({X-Request-Id}i)s\",\"response_code\":\"%(s)s\",\"request_method\":\"%(m)s\",\"request_path\":\"%(U)s\",\"request_querystring\":\"%(q)s\",\"request_timetaken\":\"%(D)s\",\"response_length\":\"%(B)s\"}"
  2>&1 | grep -v kube-probe &
 # And nginx to proxy & serve static files
 nginx -g "daemon off;" 2>&1 |grep -v kube-proxy
