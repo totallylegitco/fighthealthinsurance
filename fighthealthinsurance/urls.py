d5cc0aa5ba4a8b7fd24cb07a1dec83521f721d00
Holden Karau <holden@pigscanfly.ca>
2024-08-16 22:40:55 -0700

    Fix style for the path cache control

--- fighthealthinsurance/urls.py
+++ fighthealthinsurance/urls.py
@@ -24,15 +24,29 @@ urlpatterns = [
     path("timbit/admin/", admin.site.urls),
     path("scan", views.ProcessView.as_view(), name="scan"),
     path("server_side_ocr", views.OCRView.as_view(), name="server_side_ocr"),
-    path("", cache_control(public=True)(cache_page(60 * 60 * 2)(views.IndexView.as_view())), name="root"),
-    path("about-us", cache_control(public=True)(cache_page(60 * 60 * 2)(views.AboutView.as_view())), name="about"),
+    path(
+        "",
+        cache_control(public=True)(cache_page(60 * 60 * 2)(views.IndexView.as_view())),
+        name="root",
+    ),
+    path(
+        "about-us",
+        cache_control(public=True)(cache_page(60 * 60 * 2)(views.AboutView.as_view())),
+        name="about",
+    ),
     path("other-resources", views.OtherResourcesView.as_view(), name="other-resources"),
     path("process", views.ProcessView.as_view(), name="process"),
     path("privacy_policy", views.PrivacyPolicyView.as_view(), name="privacy_policy"),
     path("share_denial", views.ShareDenialView.as_view(), name="share_denial"),
     path("share_appeal", views.ShareAppealView.as_view(), name="share_appeal"),
     path("remove_data", views.RemoveDataView.as_view(), name="remove_data"),
-    path("tos", cache_control(public=True)(cache_page(60 * 60 * 2)(views.TermsOfServiceView.as_view())), name="tos"),
+    path(
+        "tos",
+        cache_control(public=True)(
+            cache_page(60 * 60 * 2)(views.TermsOfServiceView.as_view())
+        ),
+        name="tos",
+    ),
     path("find_next_steps", views.FindNextSteps.as_view(), name="find_next_steps"),
     path("generate_appeal", views.GenerateAppeal.as_view(), name="generate_appeal"),
     path(
@@ -41,5 +55,11 @@ urlpatterns = [
         name="appeals_json_backend",
     ),
     path("choose_appeal", views.ChooseAppeal.as_view(), name="choose_appeal"),
-    path("contact", cache_control(public=True)(cache_page(60 * 60 * 2)(views.ContactView.as_view())), name="contact"),
+    path(
+        "contact",
+        cache_control(public=True)(
+            cache_page(60 * 60 * 2)(views.ContactView.as_view())
+        ),
+        name="contact",
+    ),
 ]
