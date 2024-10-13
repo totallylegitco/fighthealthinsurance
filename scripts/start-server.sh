190b1a21e0c844f8d655a5c512cac11ef1a7ccbf
Holden Karau <holden@pigscanfly.ca>
2024-10-12 18:13:39 -0700

    Log a bit more so we can debug the S3/minio stuff incicentally.

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -17,7 +17,7 @@ export DJANGO_CONFIGURATION=${DJANGO_CONFIGURATION:-"Prod"}
 export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-"fighthealthinsurance.settings"}
 # Needed for https://github.com/MacHu-GWU/uszipcode-project/blob/21242cad7cbb7eaf73235086b89499bd90c36531/uszipcode/db.py#L23
 export HOME=$(pwd)
-gunicorn fighthealthinsurance.wsgi --user www-data --bind 0.0.0.0:8010 --workers 2 --access-logformat  "{\"actual_ip\": \"%({X-Forwarded-For}i)s\", \"remote_ip\":\"%(h)s\",\"request_id\":\"%({X-Request-Id}i)s\",\"response_code\":\"%(s)s\",\"request_method\":\"%(m)s\",\"request_path\":\"%(U)s\",\"request_querystring\":\"%(q)s\",\"request_timetaken\":\"%(D)s\",\"response_length\":\"%(B)s\", \"agent\": \"\%(a)s\"}" --error-logfile /tmp/gerror.log --access-logfile /tmp/gaccess.log 2>&1 | grep -v kube-probe  | grep -v kube-proxy &
+gunicorn fighthealthinsurance.wsgi --user www-data --bind 0.0.0.0:8010 --workers 2 --access-logformat  "{\"actual_ip\": \"%({X-Forwarded-For}i)s\", \"remote_ip\":\"%(h)s\",\"request_id\":\"%({X-Request-Id}i)s\",\"response_code\":\"%(s)s\",\"request_method\":\"%(m)s\",\"request_path\":\"%(U)s\",\"request_querystring\":\"%(q)s\",\"request_timetaken\":\"%(D)s\",\"response_length\":\"%(B)s\", \"agent\": \"\%(a)s\"}" --error-logfile /tmp/gerror.log --access-logfile /tmp/gaccess.log --capture-output --log-level debug 2>&1 | grep -v kube-probe  | grep -v kube-proxy &
 sleep 2
 # And nginx to proxy & serve static files
 tail -f /tmp/*.log |grep -v kube-probe &
