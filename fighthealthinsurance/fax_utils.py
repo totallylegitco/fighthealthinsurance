54eb9c2d3b8f364b8df5caad1a84f1d2cc8560eb
Holden Karau <holden@pigscanfly.ca>
2024-10-29 21:46:16 -0700

    Add some basic logic to chunk faxes

--- fighthealthinsurance/fax_utils.py
+++ fighthealthinsurance/fax_utils.py
@@ -5,6 +5,7 @@ from typing import Optional
 import os
 import time
 import subprocess
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
 FROM_FAX = os.getenv("FROM_FAX", "4158407591")
@@ -17,7 +18,7 @@ class FaxSenderBase(object):
     # Avoid calling 211/etc.
     special_naps = re.compile(r"^1?(\d11|\d\d\d\d11)")
 
-    def estimate_cost(self, pages: int) -> int:
+    def estimate_cost(self, destination: str, pages: int) -> int:
         return self.base_cost + self.cost_per_page * pages
 
     def parse_phone_number(self, input: str) -> str:
@@ -36,6 +37,22 @@ class FaxSenderBase(object):
         if self.special_naps.match(number_str):
             raise Exception("No calling special svc numbers")
         return number_str
+
+    def send_fax(
+        self,
+        destination: str,
+        path: str,
+        dest_name: Optional[str] = None,
+        blocking: bool = False,
+    ):
+        if blocking:
+            self.send_fax_blocking(
+                destination=destination, path=path, dest_name=dest_name
+            )
+        else:
+            self.send_fax_nonblocking(
+                destination=destination, path=path, dest_name=dest_name
+            )
 
     def send_fax_blocking(
         self, destination: str, path: str, dest_name: Optional[str] = None
@@ -233,7 +250,7 @@ class HylaFaxClient(FaxSenderBase):
             raise Exception("Can not send fax without a host to fax from")
         # Going above 9600 causes issues sometimes
         with tempfile.NamedTemporaryFile(
-            suffix=".txt", prefix="meeps", mode="w+t", delete=False
+            suffix=".txt", prefix="meeps", mode="w+t", delete=True
         ) as f:
             f.write(destination)
             f.flush()
@@ -265,3 +282,97 @@ class FaxyMcFaxFace(HylaFaxClient):
     cost_per_page = 1
     host = os.getenv("FAXYMCFAXFACE_HOST")
     max_speed = 9600
+
+
+class FlexibleFaxMagic(object):
+    """Fax interface that routes to different backends and chops pages etc."""
+
+    def __init__(self, backends: list[FaxSenderBase], max_pages=10):
+        self.backends = backends
+        self.max_pages = max_pages
+
+    def assemble_outputs(self, user_header: str, input_paths: list[str]) -> list[str]:
+        """Assemble the outputs into chunks of max_pages length"""
+        # Keep track of the total pages
+        total_input_pages = 0
+        # Start with converting everything into pdf & counting the pages
+        for input_path in input_paths:
+            command = ["pandoc", input_path, f"-o{input_path}.pdf"]
+            result = subprocess.run(command)
+            reader = PdfFileReader(f"{input_path}.pdf")
+            total_input_pages += len(reader.pages)
+        # How many chunks do we need to make
+        number_of_transmissions = 1 + int(total_input_pages / self.max_pages)
+        results: list[str] = []
+        # Iterate through making the chunks
+        input_index = 0
+        current_input_file = PdfFileReader(f"{input_path[0]}.pdf")
+        index_in_current_file = 0
+        for i in range(1, number_of_transmissions):
+            # Compute the number of pages in this transmission.
+            x_pages = self.max_pages
+            if i == number_of_transmissions:
+                x_pages = total_input_pages % self.max_pages
+            # Write out the header
+            header_path = ""
+            with tempfile.NamedTemporaryFile(
+                suffix=".txt", prefix="meeps", mode="w+t", delete=False
+            ) as f:
+                header = f"""This part of transmission {user_header} which is transmission {i} of {number_of_transmissions} with {x_pages} in this transmission in addition to the cover page [this page]."""
+                f.write(header)
+                f.sync()
+                command = ["pandoc", f.name, f"-o{f.name}.pdf"]
+                subprocess.run(command)
+                header_path = f"{f.name}.pdf"
+            with tempfile.NamedTemporaryFile(
+                suffix=".pdf", prefix="meeps", mode="w+t", delete=False
+            ) as f:
+                results.append(f.name)
+                w = PdfFileWriter(f)
+                header_reader = PdfFileReader(header_path)
+                w.addPage(header_reader.pages[0])
+                transmission_page_count = 0
+                while transmission_page_count < self.max_pages:
+                    if index_in_current_file < len(current_input_file.pages):
+                        w.addPage(current_input_file.pages[index_in_current_file])
+                        transmission_page_count += 1
+                        index_in_current_file += 1
+                    elif input_index < len(input_paths):
+                        index_in_current_file = 0
+                        input_index += 1
+                        current_input_file = PdfFileReader(
+                            f"{input_paths[input_index]}.pdf"
+                        )
+                    else:
+                        break
+        return results
+
+    def send_fax(
+        self, input_paths: list[str], destination: str, blocking: bool
+    ) -> bool:
+        import uuid
+
+        myuuid = uuid.uuid4()
+        myuuidStr = str(myuuid)
+        transmission_files = self.assemble_outputs(myuuidStr, input_paths)
+        for transmission in transmission_files:
+            r = self._send_fax(
+                path=transmission, destination=destination, blocking=blocking
+            )
+            if r is False:
+                return r
+        return True
+
+    def _send_fax(self, path: str, destination: str, blocking: bool) -> bool:
+        backend = self.backends[0]
+        page_count = len(PdfFileReader(path).pages)
+        backend_cost = backend.estimate_cost(destination, page_count)
+        for candidate in self.backends[1:]:
+            candidate_cost = candidate.estimate_cost(destination, page_count)
+            if backend_cost > candidate_cost:
+                backend = candidate
+                backend_cost = candidate_cost
+        return backend.send_fax(destination=destination, path=path, blocking=blocking)
+
+
+flexible_fax_magic = FlexibleFaxMagic([SonicFax(), FaxyMcFaxFace()])
