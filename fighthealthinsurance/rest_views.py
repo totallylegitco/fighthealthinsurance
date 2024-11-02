4eec9a60306ae44778fa2d56c9cf46ee30679651
Holden Karau <holden@pigscanfly.ca>
2024-11-02 13:05:55 -0700

    Style fix

--- fighthealthinsurance/rest_views.py
+++ fighthealthinsurance/rest_views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+
 class Ping(APIView):
     def get(self, request):
         return Response(status=status.HTTP_204_NO_CONTENT)
