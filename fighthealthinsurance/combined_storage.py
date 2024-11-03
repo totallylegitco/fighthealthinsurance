cb50c26dd854dfb4eb101dbd9672aed40a215d40
Holden Karau <holden@pigscanfly.ca>
2024-11-02 23:04:06 -0700

    Add url func

--- fighthealthinsurance/combined_storage.py
+++ fighthealthinsurance/combined_storage.py
@@ -45,6 +45,19 @@ class CombinedStorage(Storage):
                 print(f"Error saving {e} to {backend}")
         return l
 
+    def url(self, *args, **kwargs):
+        last_error = None
+        for backend in self.backends:
+            try:
+                with Timeout(2.0) as timeout_ctx:
+                    return backend.url(*args, **kwargs)
+            except Exception as e:
+                print(f"Error saving {e} to {backend}")
+        if last_error is not None:
+            raise last_error
+        else:
+            return None
+
     def exists(self, name):
         if name is None:
             return False
