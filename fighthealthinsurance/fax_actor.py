3384a4ea2596b4c5e697cf6379fd1d84b3fbd561
Holden Karau <holden@pigscanfly.ca>
2024-11-19 23:09:31 -0800

    Remove async from the fax actor (#131)
    * eh the async is more pain than it's worth right now. to do: figure out the async django issue l8r
    
    * Updating fax follow up message to be a bit more clear.
    
    * Style

--- fighthealthinsurance/fax_actor.py
+++ fighthealthinsurance/fax_actor.py
@@ -1,10 +1,10 @@
 from typing import Tuple
 import ray
 from fighthealthinsurance.fax_utils import *
-from asgiref.sync import sync_to_async
 from django.utils import timezone
 from datetime import datetime, timedelta
 import time
+import asyncio
 
 
 @ray.remote(max_restarts=-1, max_task_retries=-1)
@@ -30,7 +30,7 @@ class FaxActor:
         """Bump this to restart the fax actor."""
         return 1
 
-    def __test_create_fax_object(self, **kwargs):
+    def test_create_fax_object(self, **kwargs):
         from fighthealthinsurance.models import FaxesToSend
 
         fax = FaxesToSend.objects.create(**kwargs)
@@ -40,14 +40,10 @@ class FaxActor:
             fax.save()
         return fax
 
-    async def test_create_fax_object(self, **kwargs):
-        """Test function to create fax object. Used in test mode because of _reasons_"""
-        return await sync_to_async(self.__test_create_fax_object)(**kwargs)
+    def test_delete(self, fax):
+        return fax.delete()
 
-    async def test_delete(self, fax):
-        return await sync_to_async(fax.delete)()
-
-    def __test_migrate(self):
+    def test_migrate(self):
         from fighthealthinsurance.models import FaxesToSend
         from django.core.management import call_command
 
@@ -60,10 +56,7 @@ class FaxActor:
             print(f"Couldn't delete faxes {e}")
             call_command("migrate")
 
-    async def test_migrate(self):
-        return await sync_to_async(self.__test_migrate)()
-
-    async def send_delayed_faxes(self) -> Tuple[int, int]:
+    def send_delayed_faxes(self) -> Tuple[int, int]:
         from fighthealthinsurance.models import FaxesToSend
 
         target_time = timezone.now() - timedelta(hours=1)
@@ -74,23 +67,23 @@ class FaxActor:
         )
         t = 0
         f = 0
-        async for fax in delayed_faxes:
+        for fax in delayed_faxes:
             try:
                 print(f"Attempting to send fax {fax}")
                 t = t + 1
-                response = await self.do_send_fax_object(fax)
+                response = self.do_send_fax_object(fax)
                 print(f"Sent fax {fax} with result {response}")
             except Exception as e:
                 print(f"Error sending fax {fax}: {e}")
                 f = f + 1
         return (t, f)
 
-    async def do_send_fax(self, hashed_email: str, uuid: str) -> bool:
+    def do_send_fax(self, hashed_email: str, uuid: str) -> bool:
         # Now that we have an app instance we can import faxes to send
         from fighthealthinsurance.models import FaxesToSend
 
         fax = FaxesToSend.objects.filter(uuid=uuid, hashed_email=hashed_email).get()
-        return await self.do_send_fax_object(fax)
+        return self.do_send_fax_object(fax)
 
     def _update_fax_for_sending(self, fax):
         print(f"Recording attempt to send time")
@@ -103,7 +96,7 @@ class FaxActor:
         fax.fax_success = fax_success
         fax.save()
 
-    async def do_send_fax_object(self, fax) -> bool:
+    def do_send_fax_object(self, fax) -> bool:
         email = fax.email
         denial = fax.denial_id
         if denial is None:
@@ -115,17 +108,17 @@ class FaxActor:
             extra += f"This is regarding claim id {denial.claim_id}."
         if fax.name is not None and len(fax.name) > 2:
             extra += f"This fax is sent on behalf of {fax.name}."
-        await sync_to_async(self._update_fax_for_sending, thread_sensitive=True)(fax)
+        self._update_fax_for_sending(fax)
         print(f"Kicking of fax sending")
-        fax_sent = await flexible_fax_magic.send_fax(
-            input_paths=[fax.get_temporary_document_path()],
-            extra=extra,
-            destination=fax.destination,
-            blocking=True,
+        fax_sent = asyncio.run(
+            flexible_fax_magic.send_fax(
+                input_paths=[fax.get_temporary_document_path()],
+                extra=extra,
+                destination=fax.destination,
+                blocking=True,
+            )
         )
-        await sync_to_async(self._update_fax_for_sent, thread_sensitive=True)(
-            fax, fax_sent
-        )
+        self._update_fax_for_sent(fax, fax_sent)
         print(f"Notifing user of result {fax_sent}")
         fax_redo_link = "https://www.fighthealthinsurance.com" + reverse(
             "fax-followup",
