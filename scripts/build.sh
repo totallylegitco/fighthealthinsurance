5add21e01b8ed507bb8c917dedf7dbe58d01d2b8
Edmondo Porcu <edmondo.porcu@gmail.com>
2024-12-08 14:57:06 -0500

    Re-organizing files in the repo to keep the root folder cleaner

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -ex
 mypy -p fighthealthinsurance
-./manage.py migrate
-./manage.py makemigrations
-./manage.py migrate
-./manage.py validate_templates
-./manage.py collectstatic --no-input
+./scripts/manage.py migrate
+./scripts/manage.py makemigrations
+./scripts/manage.py migrate
+./scripts/manage.py validate_templates
+./scripts/manage.py collectstatic --no-input
 
-pushd ./static/js; npm i; npm run build; popd
+pushd ./static/js
+npm i
+npm run build
+popd
 FHI_VERSION=v0.9.4c
 export FHI_VERSION
 # Build ray cluster first so that the cluster can come up before the job that registers the workers
 source build_ray.sh
 # The raycluster operator doesn't handle upgrades well so delete + recreate instead.
 kubectl delete raycluster -n totallylegitco raycluster-kuberay || echo "No raycluster present"
-kubectl apply -f cluster.yaml
+kubectl apply -f k8s/cluster.yaml
 # Build the web app
 IMAGE=holdenk/fight-health-insurance:${FHI_VERSION}
-(docker pull "${IMAGE}" && sleep 10) || docker buildx build --platform=linux/amd64,linux/arm64 -t "${IMAGE}" . --push
-kubectl apply -f deploy.yaml
+(docker pull "${IMAGE}" && sleep 10) || docker buildx build --platform=linux/amd64,linux/arm64 -t "${IMAGE}" k8s --push
+kubectl apply -f k8s/deploy.yaml
