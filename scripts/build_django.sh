107d80eb882012a4eccb555e2d9aab56c0de293d
Holden Karau <holden@pigscanfly.ca>
2024-12-14 13:26:14 -0800

    Use pwd for build dir for django contianer

--- scripts/build_django.sh
+++ scripts/build_django.sh
@@ -5,4 +5,4 @@ BUILDX_CMD=${BUILDX_CMD:-push}
 
 # Build the web app
 IMAGE=holdenk/fight-health-insurance:${FHI_VERSION}
-(docker pull "${IMAGE}" && sleep 10) || docker buildx build --platform=linux/amd64,linux/arm64 -t "${IMAGE}" -f k8s/Dockerfile "${BUILDX_CMD}"
+(docker pull "${IMAGE}" && sleep 10) || docker buildx build --platform=linux/amd64,linux/arm64 -t "${IMAGE}" -f k8s/Dockerfile "${BUILDX_CMD}" .
