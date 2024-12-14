38c48ad142968f25d57e755f079eba266132b3eb
Holden Karau <holden@pigscanfly.ca>
2024-12-14 12:50:39 -0800

    Fix static build script to be not exit on the npm ls check

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -ex
+# We expect npm depcheck to _maybe_ fail
+set +ex
 
 JS_PATH=fighthealthinsurance/static/js
 
 pushd "${JS_PATH}"
-npm ls >/dev/null 2>&1
+npm ls >/dev/stderr 2>&1
 npm_dep_check=$?
-popd
 
-pushd "${JS_PATH}"
+set -ex
+
 if [ ${npm_dep_check} != 0 ]; then
 	npm i || echo "Can't install?" >/dev/stderr
 fi
