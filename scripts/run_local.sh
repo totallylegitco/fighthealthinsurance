7a1c70dd6e2792079f1fd4d17d769b1d574bb447
Holden Karau <holden@pigscanfly.ca>
2024-12-14 20:11:06 -0800

    Run shellcheck and update the scripts, use envsubst to reduce file changes, etc.

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# shellcheck disable=SC2068
+
+
+SCRIPT_DIR="$(dirname "$0")"
 
 # Activate the venv if present.
 if [ -f ./build_venv/bin/activate ]; then
@@ -10,7 +14,7 @@ elif [ -f ./.venv/bin/activate ]; then
 fi
 
 check_python_environment() {
-	python -c 'import configurations' 2>&1 >/dev/null
+	python -c 'import configurations'  >/dev/null 2>&1
 	python_dep_check=$?
 	if [ ${python_dep_check} != 0 ]; then
 		set +x
@@ -25,7 +29,7 @@ check_python_environment
 
 set -ex
 
-./scripts/build_static.sh
+"${SCRIPT_DIR}/build_static.sh"
 
 # Are we sort of connected to the backend?
 # if kubectl get service -n totallylegitco vllm-health-svc; then
