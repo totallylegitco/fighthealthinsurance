23c702660ab5502d4e43838d20dd48f6073cc8df
Holden Karau <holden@pigscanfly.ca>
2024-12-18 17:33:02 -0800

    Add staging deploys

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,15 +4,19 @@ set -ex
 cd /opt/fighthealthinsurance
 export DJANGO_CONFIGURATION=${DJANGO_CONFIGURATION:-"Prod"}
 export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-"fighthealthinsurance.settings"}
-# Run migrations on primary only
-if [ -n "$PRIMARY" ]; then
+# Run migrations on the migrations container only
+if [ -n "$MIGRATIONS" ]; then
   python manage.py migrate
   python manage.py loaddata initial
   python manage.py loaddata followup
   python manage.py loaddata plan_source
   python manage.py ensure_adminuser --no-input
+  sleep 10
+  exit 0
+elif [ -n "$POLLING_ACTORS" ]; then
+  # Some for polling actors
   python manage.py launch_polling_actors || (echo "Error starting ray actor?" && sleep 480)
-  sleep 60
+  sleep 10
   exit 0
 fi
 # Start gunicorn
@@ -21,6 +25,4 @@ HOME=$(pwd)
 export HOME
 sudo -u www-data uvicorn fighthealthinsurance.asgi:application --host 0.0.0.0 --port 8010 --workers 2 --access-log --proxy-headers --access-log  2>&1 | grep -v kube-probe  | grep -v kube-proxy &
 sleep 2
-# And nginx to proxy & serve static files
-tail -f /tmp/*.log |grep -v kube-probe &
 nginx -g "daemon off;" 2>&1 |grep -v kube-proxy |grep -v kube-probe
