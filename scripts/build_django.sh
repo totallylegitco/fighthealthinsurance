b22f4c8b1c23fb586e8e307be80bf7eac8fe1f65
Holden Karau <holden@pigscanfly.ca>
2025-01-07 10:50:08 -0800

    Don't pull instead use docker manifest inspect

--- scripts/build_django.sh
+++ scripts/build_django.sh
@@ -8,4 +8,4 @@ PLATFORM=${PLATFORM:-linux/amd64,linux/arm64}
 
 # Build the web app
 IMAGE=holdenk/fight-health-insurance:${FHI_VERSION}
-(docker pull "${IMAGE}" && sleep 10) || docker buildx build --platform="${PLATFORM}" -t "${IMAGE}" -f k8s/Dockerfile "--${BUILDX_CMD}" .
+(docker manifest inspect "${IMAGE}" && sleep 1) || docker buildx build --platform="${PLATFORM}" -t "${IMAGE}" -f k8s/Dockerfile "--${BUILDX_CMD}" .
