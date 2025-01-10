b4eae3c68877e1a0d6502d71f9b7b00295bfee3d
Holden Karau <holden@pigscanfly.ca>
2025-01-10 15:29:39 -0800

    Add sentry (#184)

--- fighthealthinsurance/settings.py
+++ fighthealthinsurance/settings.py
@@ -58,6 +58,25 @@ class Base(Configuration):
 
     ALLOWED_HOSTS: list[str] = ["*"]
 
+    sentry_endpoint = os.getenv("SENTRY_ENDPOINT")
+
+    if sentry_endpoint:
+
+        import sentry_sdk
+
+        sentry_sdk.init(
+            dsn=sentry_endpoint,
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for tracing.
+            traces_sample_rate=1.0,
+            _experiments={
+                # Set continuous_profiling_auto_start to True
+                # to automatically start the profiler on when
+                # possible.
+                "continuous_profiling_auto_start": True,
+            },
+        )
+
     # Application definition
 
     SITE_ID = 1
