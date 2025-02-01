c6ae811eb6edc0152d5d4389610b46e331e8f3ec
Holden Karau <holden@pigscanfly.ca>
2025-02-01 13:14:45 -0800

    Style

--- fighthealthinsurance/auth/auth_forms.py
+++ fighthealthinsurance/auth/auth_forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 
+
 class DomainAuthenticationForm(AuthenticationForm):
     domain = forms.CharField(required=True)
