7b011b6acb2c58327ce6e9510399382ff4fae492
Holden Karau <holden@pigscanfly.ca>
2025-02-01 20:05:16 -0800

    Quick notes

--- fighthealthinsurance/auth/auth_utils.py
+++ fighthealthinsurance/auth/auth_utils.py
@@ -1,6 +1,9 @@
+# Different clinics will have different "domains" and the django username will be the user visible username + a clinic name seperated by a panda unicode
+
 def validate_username(username: str) -> bool:
     return "🐼" not in username
 
 
 def combine_domain_and_username(username: str, domain: str) -> str:
+    # TODO: Resolve domain str to ID since folks might have the domain change.
     return f"{username}🐼{domain}"
