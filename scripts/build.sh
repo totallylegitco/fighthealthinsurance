440dee4975b7c7d4246e0334c564466e54cda4fb
Holden Karau <holden@pigscanfly.ca>
2025-02-03 10:16:48 -0800

    2nd factor auth bump to 0.10

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ BUILDX_CMD=${BUILDX_CMD:-push}
 
 source "${SCRIPT_DIR}/setup_templates.sh"
 
-FHI_VERSION=v0.9.25a
+FHI_VERSION=v0.10.1a
 RAY_BASE=${RAY_BASE:-totallylegitco/fhi-ray}
 FHI_BASE=${FHI_BASE:-totallylegitco/fhi-base}
 
