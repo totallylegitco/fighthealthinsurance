7cee86c5cab1af67b09df177ae3b6aa38b58830c
Holden Karau <holden@pigscanfly.ca>
2025-02-05 11:15:59 -0800

    Bump version

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ BUILDX_CMD=${BUILDX_CMD:-push}
 
 source "${SCRIPT_DIR}/setup_templates.sh"
 
-FHI_VERSION=v0.10.4a
+FHI_VERSION=v0.10.4b
 MYORG=${MYORG:-holdenk}
 RAY_BASE=${RAY_BASE:-${MYORG}/fhi-ray}
 FHI_BASE=${FHI_BASE:-${MYORG}/fhi-base}
