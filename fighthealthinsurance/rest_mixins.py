4341a524f7e0d195424471a150c6a473e191d742
faiz-ul-hassan747284 <faiz-ul-hassan747284@gmail.come>
2025-02-28 05:16:16 -0800

    fix serializer

--- fighthealthinsurance/rest_mixins.py
+++ fighthealthinsurance/rest_mixins.py
@@ -2,20 +2,23 @@ import typing
 from rest_framework import status
 from rest_framework.serializers import Serializer
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 
 
 class SerializerMixin:
     serializer_class: typing.Optional[typing.Type[Serializer]] = None
 
-    def get_serializer_class(self) -> typing.Type[Serializer]:
+    def get_serializer_class(self):
         if self.serializer_class is None:
             raise ValidationError("serializer_class must not be None")
         return self.serializer_class
 
-    def deserialize(self, data=None) -> Serializer:
-        serializer_cls = self.get_serializer_class()
-        return serializer_cls(data=data)
+    def deserialize(self, data=None):
+        if self.get_serializer_class() is None:
+            raise ValueError("serializer_class must be defined and not None")
+        else:
+            return self.get_serializer_class()(data=data)
 
 
 class CreateMixin(SerializerMixin):
@@ -38,10 +41,23 @@ class CreateMixin(SerializerMixin):
 
 class DeleteMixin(SerializerMixin):
     def perform_delete(self, request, serializer):
-        raise NotImplementedError("Subclasses must implement perform_delete()")
+        pass
 
-    def delete(self, request, *args, **kwargs) -> Response:
+    def delete(self, request, *args, **kwargs):
+        """For some reason"""
         serializer = self.deserialize(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_delete(request, serializer, *args, **kwargs)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class DeleteOnlyMixin:
+    """Extra mixin that allows router display for delete-only resources"""
+
+    def list(self, request, *args, **kwargs):
+        # For some reason, delete resources don't show if there's not an
+        # associated endpoint for working with their related data. So,
+        # this adds one that 404s whenever its used until we can come up
+        # with a better solution (or figure out what's wrong)
+
+        return Response(status=status.HTTP_404_NOT_FOUND)
