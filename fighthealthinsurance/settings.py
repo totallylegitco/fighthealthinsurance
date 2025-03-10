c845e354005e9aa628cc5bca5202aed51c6fa11e
Holden Karau <holden@pigscanfly.ca>
2025-03-09 22:23:43 -0700

    allow ci host in dev

--- fighthealthinsurance/settings.py
+++ fighthealthinsurance/settings.py
@@ -390,6 +390,7 @@ class Dev(Base):
     CSRF_TRUSTED_ORIGINS = [
         "https://fightpaperwork.com",
         "https://localhost:3000",
+        "http://localhost:3000",
         "https://localhost:8000",
     ]
     DEFF_SALT = os.getenv("DEFF_SALT", "dev-salt")
